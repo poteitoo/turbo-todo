5b08ad53865cfd1272a99496a0548385be36c719
poteitoo <1105tigery@gmail.com>
2023-05-28 14:39:05 +0000

    fix navbar

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,23 +1,24 @@
 <div class="full bg-gradient-blue drawer-mobile drawer">
 	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content flex flex-col p-5 md:pl-0">
+	<div class="drawer-content flex flex-col p-5 lg:pl-0">
 		<!-- {/* Navbar */} -->
 		<div class="navbar w-full rounded-md bg-base-100 shadow-md">
 			<div class="flex-none lg:hidden">
-				<label for="my-drawer-3" class="btn-ghost btn-square btn">
-					<!-- <svg
+				<label for="my-drawer-3" class="btn-ghost btn-square btn"
+					><svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
-						class="inline-block h-6 w-6 stroke-current"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="h-6 w-6"
 					>
 						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="M4 6h16M4 12h16M4 18h16"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
 						/>
-					</svg> -->
+					</svg>
 				</label>
 			</div>
 			<div class="mx-2 flex-1 px-2">Navbar Title</div>
@@ -38,7 +39,7 @@
 	</div>
 	<div class="drawer-side md:m-5">
 		<label for="my-drawer-3" class="drawer-overlay" />
-		<ul class="menu w-80 rounded-md bg-base-100 p-4 shadow-md">
+		<ul class="menu w-80 rounded-md bg-base-100 p-4 lg:shadow-md">
 			<!-- {/* Sidebar content here */} -->
 			<li>
 				<a href="/dashboard">Sidebar Item 1</a>
