de95f3472f43487428210fecfac7ee2bd66a4f8f
poteitoo <1105tigery@gmail.com>
2023-05-28 08:30:46 +0000

    feat passwordless authentication

--- modules/Auth/Forms/signIn.tsx
+++ modules/Auth/Forms/signIn.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { useIsMounted } from "@/hooks/useIsMounted";
+import { signIn } from "next-auth/react";
 import { useForm } from "react-hook-form";
 
 export type SignInFormType = {
+  email: string;
   username: string;
   password: string;
   remember: boolean;
@@ -20,75 +22,50 @@ export const SignInForm = () => {
   });
   const mounted = useIsMounted();
 
-  const onSubmit = (data: any) => console.log(data);
+  const onSubmit = (data: SignInFormType) => {
+    signIn("email", { email: data.email, redirect: true });
+    console.log(data);
+  };
 
   if (!mounted) return null;
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col gap-6 card card-compact glass w-96 rounded-md shadow-md p-8 pt-6 mb-4"
-    >
-      <h2 className="font-bold text-xl">Sign in to your account</h2>
+    <div className="card card-compact glass w-96 rounded-md shadow-md p-8 pt-6 mb-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
+        <h2 className="font-bold text-xl">Sign in to your account</h2>
+        {/* email */}
+        <div>
+          <label htmlFor="email" className="block mb-1 font-semibold">
+            Email
+          </label>
+          <input
+            type="email"
+            className="input input-bordered w-full"
+            placeholder="poepoe"
+            id="email"
+            {...register("email", { required: true })}
+          />
+          {errors.email && (
+            <p className="text-red-600 font-medium mt-1">
+              メールアドレスを入力してください
+            </p>
+          )}
+        </div>
 
-      {/* username */}
-      <div>
-        <label htmlFor="username" className="block mb-1 font-semibold">
-          Username
-        </label>
-        <input
-          type="text"
-          className="input input-bordered w-full"
-          placeholder="poepoe"
-          id="username"
-          {...register("username", { required: true })}
-        />
-        {errors.username && (
-          <p className="text-red-600 font-medium mt-1">
-            ユーザー名を入力してください
-          </p>
+        {/* submit */}
+        <button className="btn" type="submit">
+          Sign In
+        </button>
+        {errors.root && (
+          <p className="text-red-600 font-medium">{errors.root.message}</p>
         )}
-      </div>
+      </form>
 
-      {/* password */}
-      <div>
-        <label htmlFor="password" className="block mb-1 font-semibold">
-          Password
-        </label>
-        <input
-          type="password"
-          className="input input-bordered w-full"
-          placeholder="*****"
-          id="password"
-          {...register("password", { required: true })}
-        />
-        {errors.password && (
-          <p className="text-red-600 font-medium mt-1">
-            パスワードを入力してください
-          </p>
-        )}
-      </div>
-
-      {/* checkbox */}
-      <div>
-        <input
-          type="checkbox"
-          className="checkbox mr-3"
-          id="remember"
-          {...register("remember")}
-        />
-        <label htmlFor="remember" className="mb-1 font-medium">
-          Remember me
-        </label>
-      </div>
-
-      {/* submit */}
-      <button className="btn" type="submit">
-        Sign In
+      {/* SSO */}
+      <div className="divider">OR</div>
+      <button className="btn" onClick={() => signIn("github")}>
+        Github
       </button>
-      {errors.root && (
-        <p className="text-red-600 font-medium">{errors.root.message}</p>
-      )}
-    </form>
+    </div>
   );
 };
--- modules/Auth/authOptions.ts
+++ modules/Auth/authOptions.ts
@@ -1,11 +1,13 @@
 import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import EmailProvider from "next-auth/providers/email";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-  },
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
@@ -23,4 +25,15 @@ export const authOptions: NextAuthOptions = {
       from: process.env.EMAIL_FROM,
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile, email }) {
+      if (!user.email) return "/";
+
+      return "/dashboard";
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+  },
 };
